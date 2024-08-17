bea784c0f70e0e04de81b1c2d32486488c66475e
Roma Korinenko <65089062+romakorinenko@users.noreply.github.com>
2024-08-17 20:21:43 +0300

    hw03 - write chessboard

--- hw03_chessboard/go.mod
+++ hw03_chessboard/go.mod
@@ -1,3 +1,3 @@
-module github.com/fixme_my_friend/hw03_chessboard
+module github.com/romakorinenko/hw-test/hw03_chessboard
 
 go 1.20
--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,51 @@
 package main
 
+import "fmt"
+
+const (
+	black = "#"
+	white = " "
+)
+
 func main() {
-	// Place your code here.
+	var size int
+	fmt.Print("Enter chessboard size: ")
+	if _, err := fmt.Scanf("%d", &size); err != nil {
+		fmt.Printf("getting chessboard size: %v", err)
+	}
+
+	chessboard(size)
+}
+
+func chessboard(size int) string {
+	var res string
+	for str := 0; str < size; str++ {
+		res += chessString(size, str%2 == 0)
+	}
+
+	return res
+}
+
+func chessString(size int, isFirstBlack bool) string {
+	var res string
+
+	if isFirstBlack {
+		for cell := 0; cell < size; cell++ {
+			if cell%2 == 0 {
+				res += black
+			} else {
+				res += white
+			}
+		}
+	} else {
+		for cell := 0; cell < size; cell++ {
+			if cell%2 == 1 {
+				res += black
+			} else {
+				res += white
+			}
+		}
+	}
+
+	return res + "\n"
 }
